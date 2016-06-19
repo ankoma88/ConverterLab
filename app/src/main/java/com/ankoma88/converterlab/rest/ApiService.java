7861bf00dc3119c4fb1eb793baa02d71c92afdcf
ankoma88 <anton.komarovskyi@gmail.com>
2016-06-19 13:26:14 +0300

    ignore

--- app/src/main/java/com/ankoma88/converterlab/rest/ApiService.java
+++ app/src/main/java/com/ankoma88/converterlab/rest/ApiService.java
@@ -6,6 +6,7 @@ import com.ankoma88.converterlab.rest.models.BaseModel;
 import retrofit.Callback;
 import retrofit.http.GET;
 
+
 public interface ApiService {
 
     @GET("/ru/public/currency-cash.json")
